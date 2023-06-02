573225fbe20602996b05bd51f2a9623c816db212
kernelsam <kernelsam@users.noreply.github.com>
2023-06-02 19:08:53 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.2

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,12 @@
-// 0.0.1
-// Created by make-go-github-file.yaml on Fri Mar 31 19:47:08 UTC 2023
+// 0.0.2
+// Created by make-go-github-file.yaml on Fri Jun  2 19:08:53 UTC 2023
 package cmd
 
-var githubDate string = "2023-03-31"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.0.1"
-var githubRefName string = "0.0.1"
-var githubRepository string = "Senzing/view-xterm"
-var githubRepositoryName string = "view-xterm"
-var githubVersion string = "0.0.1"
+var githubDate            string = "2023-06-02"
+var githubIteration       string = "0"
+var githubRef             string = "refs/tags/0.0.2"
+var githubRefName         string = "0.0.2"
+var githubRepository      string = "Senzing/view-xterm"
+var githubRepositoryName  string = "view-xterm"
+var githubVersion         string = "0.0.2"
+
